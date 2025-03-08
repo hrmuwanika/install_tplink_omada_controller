dff22b22028007118448d91e9f801bbe0e07bd94
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-03-08 11:19:28 +0200

    Update install-omada-on-alma-linux.sh

--- install-omada-on-alma-linux.sh
+++ install-omada-on-alma-linux.sh
@@ -20,11 +20,20 @@ yum update -y
 # Installl Java 11
 sudo yum install –y java-11-openjdk.x86_64
 
-# Install Jvsc
-
 # Download mongodb
-sudo wget https://repo.mongodb.org/yum/redhat/7/mongodb-org/4.4/x86_64/RPMS/mongodb-org-4.4.29-1.el7.x86_64.rpm
-sudo rpm -ivh mongodb-org-4.4.29-1.el7.x86_64.rpm
+cat <<"EOF" | tee /etc/yum.repos.d/mongodb-org-7.0.repo
+[mongodb-org-7.0]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/9/mongodb-org/7.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://pgp.mongodb.com/server-7.0.asc
+EOF
+
+sudo dnf repolist
+sudo dnf install mongodb-org mongodb-mongosh
+sudo systemctl enable --now mongod
+sudo systemctl status mongod
 
 # download and install omada installer
 wget -c ${OMADAURL} -O - | tar -xz 
