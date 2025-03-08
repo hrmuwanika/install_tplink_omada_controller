8ec83a3e9199238d23b4180ac05bbf35eda4050f
Henry Robert Muwanika <hrmuwanika@gmail.com>
2025-03-08 22:12:05 +0200

    Update install-omada-on-alma-linux.sh

--- install-omada-on-alma-linux.sh
+++ install-omada-on-alma-linux.sh
@@ -1,24 +1,14 @@
 #!/bin/bash
 
 # Omada Controller installer sources: https://community.tp-link.com/en/business/forum/topic/548752
-# Linux:            https://static.tp-link.com/upload/software/2025/202501/20250109/Omada_SDN_Controller_v5.15.8.2_linux_x64.tar.gz
-
-# Info for RHEL and Omada Software Controller
-#   Omada Software Controller require jsvc but on Red Hat 8 jsvc is not available per defauls/for free
-#   you have to buy a license.
-OMADAURL="https://static.tp-link.com/upload/software/2025/202501/20250109/Omada_SDN_Controller_v5.15.8.2_linux_x64.tar.gz" && echo "URL set: ${OMADAURL}"
-
-# Set vars
-OMADATARGZ=$(echo "${OMADAURL}" | awk '{split($0,a,"/"); print a[9]}') && echo "Omada installer filename set: ${OMADATARGZ}"
-MYINSTALLERFOLDER=$(echo "${OMADATARGZ}" | sed 's/\.tar\.gz//') && echo "Omada install folder set: ${MYINSTALLERFOLDER}"
-MYOMADAVERSION=$(echo "${OMADATARGZ}" | awk '{split($0,a,"_"); print a[4]}') && echo "Omada version: ${MYOMADAVERSION}"
-OMADADEST="/opt/tplink/EAPController"
+# Linux: https://static.tp-link.com/upload/software/2025/202501/20250109/Omada_SDN_Controller_v5.15.8.2_linux_x64.tar.gz
 
 # yum update
 yum update -y
 
 # Installl Java 11
-sudo yum install –y java-11-openjdk.x86_64
+sudo yum -y install java-11-openjdk.x86_64
+sudo yum -y install http://repo.iotti.biz/CentOS/8/x86_64/apache-commons-daemon-jsvc-1.2.2-5.el8.lux.x86_64.rpm
 
 # Download mongodb
 cat <<"EOF" | tee /etc/yum.repos.d/mongodb-org-7.0.repo
@@ -30,18 +20,28 @@ enabled=1
 gpgkey=https://pgp.mongodb.com/server-7.0.asc
 EOF
 
-sudo dnf repolist
-sudo dnf install mongodb-org mongodb-mongosh
+sudo dnf -y install mongodb-org 
 sudo systemctl enable --now mongod
-sudo systemctl status mongod
+sudo systemctl start mongod
+
+# Now open your firewall ports
+dnf -y install firewalld
+firewall-cmd --zone=public --add-port=8088/tcp --permanent     # http connection
+firewall-cmd --zone=public --add-port=8043/tcp --permanent     # https connection
+firewall-cmd --zone=public --add-port=29810/udp --permanent    # EAP Discovery
+firewall-cmd --zone=public --add-port=29811/tcp --permanent    # EAP Management
+firewall-cmd --zone=public --add-port=29812/tcp --permanent    # EAP Adoption
+firewall-cmd --zone=public --add-port=29813/tcp --permanent    # EAP Upgrades and initialisation check
+firewall-cmd --zone=public --add-port=29814/tcp --permanent    # EAP Upgrades and initialisation check
+firewall-cmd --reload
 
 # download and install omada installer
-wget -c ${OMADAURL} -O - | tar -xz 
+wget -c https://static.tp-link.com/upload/software/2025/202501/20250109/Omada_SDN_Controller_v5.15.8.2_linux_x64.tar.gz -O - | tar -xz 
 
 # Make scripts and binaries executable    
-chmod +x ${MYINSTALLERFOLDER}/*.sh
-chmod +x ${MYINSTALLERFOLDER}/bin/*
+chmod +x Omada_SDN_Controller_v5.15.8.2_linux_x64/*.sh
+chmod +x Omada_SDN_Controller_v5.15.8.2_linux_x64/bin/*
 
 # Install omada controller, by default it is enabled on boot
-cd ${MYINSTALLERFOLDER}
+cd Omada_SDN_Controller_v5.15.8.2_linux_x64/
 ./install.sh
